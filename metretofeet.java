545c2def769e98066bd74394e07a689692c27072
Lakshay Sharma <lakshayiitian12@gmail.com>
2025-08-07 12:03:40 +0530

    Create metretofeet.java

---
+++ metretofeet.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int feet=sc.nextInt();
+        double meters=3.28*feet;
+        System.out.printIn("The conversion from meter to feet is:"meters+"feet");
+    }
+}

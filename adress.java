a87987515ed9af2da8226c01c1fae19440591b36
Lakshay Sharma <lakshayiitian12@gmail.com>
2025-08-07 11:26:36 +0530

    Create adress.java

---
+++ adress.java
@@ -0,0 +1,7 @@
+public class Adress{
+    public static void main(String[]args) {
+        System.out.printLn("My Adress is:");
+         System.out.printLn("122/22:");
+          System.out.printLn("krishna nagar:");
+    }
+}

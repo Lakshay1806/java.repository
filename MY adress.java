f658dd2d65920572e129dc94bee6689e7ed11d47
Lakshay Sharma <lakshayiitian12@gmail.com>
2025-11-19 23:18:16 +0530

    Create MY adress.java

---
+++ MY adress.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("My address is: ");
+        System.out.println("84,Street no 12 Sarojini Park");
+        System.out.println("Shastri nagar");
+    }
+}

e20ead2f4b79fa0d7c1f57c1a7be1aa78ad1e7ac
Lakshay Sharma <lakshayiitian12@gmail.com>
2025-11-19 23:46:48 +0530

    Add Parent and Child classes with final method

---
+++ ParentChild.java
@@ -0,0 +1,19 @@
+class Parent {
+    final void display() {
+        System.out.println("Final method: cannot be overridden");
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("Child method");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Child obj = new Child();
+        obj.display();
+        obj.show();
+    }
+}
